a8444fd36c09b0f3790043dc49ffb649e902ebbb
MarcoVelazquez <42878003+MarcoVelazquez@users.noreply.github.com>
2018-09-30 12:02:39 -0700

    Update Colas_2.py

--- Colas_2.py
+++ Colas_2.py
@@ -1,66 +1,88 @@
-
+item = 0
+i = 0
 def New_Cola():
 	Cola = [0,0,0,0,0]
 	i = 4
 	return Cola,i
 
 def Pop(Cola_,i):
-	del(Cola_[4])
+	Cola_[4] = Cola_[3]
+	Cola_[3] = Cola_[2]
+	Cola_[2] = Cola_[1]
+	Cola_[1] = Cola_[0]
+	Cola_[0] = 0
 	i += 1
-	return Cola_, i
-
-def Push(Cola_,item,i):
-	if i == 4:
-		Cola_[i] = item
-		i -= 1
-		
-	elif i == 3:
-		Cola_[i] = item
-		i -= 1
-	elif i == 2:
-		Cola_[i] = item
-		i -= 1
-	elif i == 1:
-		Cola_[i] = item
-		i -= 1
-	elif i == 0:
-		Cola_[i] = item
-		i -= 1
-	elif i < 0:
-		Cola_[4] = Cola_[3]
-		Cola_[3] = Cola_[2]
-		Cola_[2] = Cola_[1]
-		Cola_[1] = Cola_[0]
-		Cola_[0] = item
-		
-
-	return Cola_,i
-
-Cola_,i = New_Cola()
-print(Cola_)
-print
-Cola_,i = Push(Cola_,1,i)
-print(Cola_)
-print
-Cola_,i = Push(Cola_,2,i)
-print(Cola_)
-print
-Cola_,i = Push(Cola_,3,i)
-print(Cola_)
-print
-Cola_,i = Push(Cola_,4,i)
-print(Cola_)
-print
-Cola_,i = Push(Cola_,5,i)
-print(Cola_)
-print
-Cola_,i = Push(Cola_,6,i)
-print(Cola_)
-
 
 
 
 	
+	return Cola_, i
 
+def Push(Cola_,item,i):
+	if i == 4:
+		print("Ingresa el numero")
+		item = int(input())
+		Cola_[i] = item
+		i -= 1
+		
+	elif i == 3:
+		print("Ingresa el numero")
+		item = int(input())
+		Cola_[i] = item
+		i -= 1
+	elif i == 2:
+		print("Ingresa el numero")
+		item = int(input())
+		Cola_[i] = item
+		i -= 1
+	elif i == 1:
+		print("Ingresa el numero")
+		item = int(input())
+		Cola_[i] = item
+		i -= 1
+	elif i == 0:
+		print("Ingresa el numero")
+		item = int(input())
+		Cola_[i] = item
+		i -= 1
 
+		
+
+	return Cola_,i,item
+
+def Menu(item,i):
+	while True:
+		print("1.-Crear cola")
+		print("2.-Anadir un numero a la cola")
+		print("3.-Sacar un numero de la cola")
+		print("4.-Imprimir cola")
+
+		Opcion = int(input())
+
+		if Opcion == 1:
+			Cola_,i = New_Cola()
+		elif Opcion == 2:
+			if i == 4:
+				Cola_,i,item = Push(Cola_,item,i)
+			elif i == 3:
+				Cola_,i,item = Push(Cola_,item,i)
+			elif i == 2:
+				Cola_,i,item = Push(Cola_,item,i)
+			elif i == 1:
+				Cola_,i,item = Push(Cola_,item,i)
+			elif i == 0:
+				Cola_,i,item = Push(Cola_,item,i)
+			else:
+				print("Cola llena")
+				
+		elif Opcion == 3:
+			Cola_,i = Pop(Cola_,i)
+			if i > 4:
+				print("VACIA")
+			
+		elif Opcion == 4:
+			print(Cola_)
+			return 0
+
+Menu(item,i)
 
